b4319e7528191e216883a05385843847e44be346
unknown <s.adeboye@outlook.com>
2024-03-20 05:01:41 +0000

    added budget interface

---
+++ Interfaces/IBudget.cs
@@ -0,0 +1,13 @@
+﻿using AdCampaigner.Models.Budget;
+
+namespace AdCampaigner.Interfaces
+{
+    public interface IBudget
+    {
+        Task<IEnumerable<Budget>> GetAllBudgetsAsync();
+        Task<Budget> GetBudgetByIdAsync(int id);
+        Task CreateBudgetAsync(Budget budget);
+        Task UpdateBudgetAsync(int id, Budget budget);
+        Task DeleteBudgetAsync(int id);
+    }
+}
