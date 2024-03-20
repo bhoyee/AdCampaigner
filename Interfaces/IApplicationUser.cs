a5afeac1cb39df258dbb01376c80e9f238f8dd43
unknown <s.adeboye@outlook.com>
2024-03-20 05:18:38 +0000

    added interface for users

---
+++ Interfaces/IApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AdCampaigner.Models.ApplicationUser;
+
+namespace AdCampaigner.Interfaces
+{
+    public interface IApplicationUser
+    {
+        Task<IEnumerable<ApplicationUser>> GetAllUsersAsync();
+        Task<ApplicationUser> GetUserByIdAsync(string id);
+        Task CreateUserAsync(ApplicationUser user);
+        Task UpdateUserAsync(string id, ApplicationUser user);
+        Task DeleteUserAsync(string id);
+    }
+}
