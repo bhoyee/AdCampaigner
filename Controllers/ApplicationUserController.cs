e91a99fa2bf4766245e861f7e772a5e89d2d4439
unknown <s.adeboye@outlook.com>
2024-03-20 05:18:01 +0000

    added usercontroller

---
+++ Controllers/ApplicationUserController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AdCampaigner.Interfaces;
+using AdCampaigner.Models.ApplicationUser;
+
+namespace AdCampaigner.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController : ControllerBase
+    {
+        private readonly IApplicationUser _userService;
+
+        public ApplicationUserController(IApplicationUser userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ApplicationUser>>> GetUsers()
+        {
+            var users = await _userService.GetAllUsersAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ApplicationUser>> GetUser(string id)
+        {
+            var user = await _userService.GetUserByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ApplicationUser>> CreateUser(ApplicationUser user)
+        {
+            await _userService.CreateUserAsync(user);
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(string id, ApplicationUser user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+            await _userService.UpdateUserAsync(id, user);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            await _userService.DeleteUserAsync(id);
+            return NoContent();
+        }
+    }
+}
