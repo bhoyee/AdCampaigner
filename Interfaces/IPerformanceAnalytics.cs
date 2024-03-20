36321a06340855ccde77fcb2ccf5a180f825296c
unknown <s.adeboye@outlook.com>
2024-03-20 05:08:48 +0000

    added IPerformanceAnalytics interface

---
+++ Interfaces/IPerformanceAnalytics.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AdCampaigner.Models.PerformanceAnalytics;
+
+namespace AdCampaigner.Interfaces
+{
+    public interface IPerformanceAnalytics
+    {
+        Task<IEnumerable<PerformanceAnalytics>> GetAllPerformanceAnalyticsAsync();
+        Task<PerformanceAnalytics> GetPerformanceAnalyticsByIdAsync(int id);
+        Task CreatePerformanceAnalyticsAsync(PerformanceAnalytics analytics);
+        Task UpdatePerformanceAnalyticsAsync(int id, PerformanceAnalytics analytics);
+        Task DeletePerformanceAnalyticsAsync(int id);
+    }
+}
