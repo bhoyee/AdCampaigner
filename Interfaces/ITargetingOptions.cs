00b53e900ec6c30f852500ee68c2826d2b6829ef
unknown <s.adeboye@outlook.com>
2024-03-20 05:19:57 +0000

    added targetoption interface

---
+++ Interfaces/ITargetingOptions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AdCampaigner.Models.TargetingOptions;
+
+namespace AdCampaigner.Interfaces
+{
+    public interface ITargetingOptions
+    {
+        Task<IEnumerable<TargetingOptions>> GetAllTargetingOptionsAsync();
+        Task<TargetingOptions> GetTargetingOptionsByIdAsync(int id);
+        Task CreateTargetingOptionsAsync(TargetingOptions targetingOptions);
+        Task UpdateTargetingOptionsAsync(int id, TargetingOptions targetingOptions);
+        Task DeleteTargetingOptionsAsync(int id);
+    }
+}
