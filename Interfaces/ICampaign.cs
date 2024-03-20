bbab7cf4547d6a30954113cc931d627508c54fcd
unknown <s.adeboye@outlook.com>
2024-03-20 00:47:34 +0000

    added Campaign interface

---
+++ Interfaces/ICampaign.cs
@@ -0,0 +1,13 @@
+﻿using AdCampaigner.Models.Campaigns;
+
+namespace AdCampaigner.Interfaces
+{
+    public interface ICampaign
+    {
+        Campaign GetById(int id);
+        IEnumerable<Campaign> GetAll();
+        void Create(Campaign campaign);
+        void Update(Campaign campaign);
+        void Delete(int id);
+    }
+}
