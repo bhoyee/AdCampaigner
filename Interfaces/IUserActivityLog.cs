38ba0ff7e3eabe5105edc29dc7d5906448b33820
unknown <s.adeboye@outlook.com>
2024-03-20 05:27:25 +0000

    add activiitylogg interface

---
+++ Interfaces/IUserActivityLog.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AdCampaigner.Models.ApplicationUser;
+
+namespace AdCampaigner.Interfaces
+{
+    public interface IUserActivityLog
+    {
+        Task<IEnumerable<UserActivityLog>> GetAllUserActivityLogsAsync();
+        Task<UserActivityLog> GetUserActivityLogByIdAsync(int id);
+        Task CreateUserActivityLogAsync(UserActivityLog activityLog);
+        // Add additional methods as needed
+    }
+}
